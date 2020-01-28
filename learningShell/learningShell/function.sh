daf52cac26e56004ab10009d2100495c6b356185
Ubuntu <ubuntu@ip-172-31-40-198.us-east-2.compute.internal>
2020-01-28 17:06:12 +0000

    function shell

---
+++ learningShell/learningShell/function.sh
@@ -0,0 +1,22 @@
+#In this exercise, you will need to write a function called ENGLISH_CALC which can process sentences such as:
+#'3 plus 5', '5 minus 1' or '4 times 6' and print the results as: '3 + 5 = 8', '5 - 1 = 4' or '4 * 6 = 24' respectively.
+
+#!/bin/bash
+# enter your function code here
+function ENGLISH_CALC(){
+  a=$1
+  b=$3
+  signn=$2
+  if [ $signn == "plus" ]; then
+    echo "$a + $b = $(($a+$b))"
+  elif [ $signn == "minus" ]; then
+    echo "$a - $b = $(($a-$b))"
+  elif [ $signn == "times" ]; then
+    echo "$a * $b = $(($a*$b))"
+  fi
+}
+
+# testing code
+ENGLISH_CALC 3 plus 5
+ENGLISH_CALC 5 minus 1
+ENGLISH_CALC 4 times 6
